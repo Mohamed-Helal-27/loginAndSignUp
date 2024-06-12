1f85661e7d8828e85e0face6c75b2686a6019649
helal <100865733+Mohamed-Helal-27@users.noreply.github.com>
2024-06-12 03:37:42 +0300

    create repo app on githup

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts') ;
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
+
+
+const app = express();
+// Passport Config
+require('./config/passport')(passport);
+
+//DB Config
+const db = require('./config/keys').MongoURI;
+
+//connect to mongo
+mongoose.connect(db)
+.then(()=>console.log('mongoDb connected done...'))
+.catch(err => console.log(err))
+;
+
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine','ejs')
+
+//Bodyparser
+app.use(express.urlencoded({extended: false}));
+
+//Expresss Session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect flash
+app.use(flash());
+//global vars
+app.use((req,res,next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+//Routes
+
+app.use('/',require('./routes/index'));
+
+app.use('/users',require('./routes/users'));
+
+
+
+const PORT = process.env.port || 5000 ; 
+app.listen(PORT, console.log(`server run on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI:'mongodb+srv://helal2720:2720MK@firstapp.qylug69.mongodb.net/?retryWrites=true&w=majority&appName=firstApp'
+}
